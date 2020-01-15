9de61ba06cd0ef804c22ec672b33c10daaa1700f
omegacat <bmbusaf@yahoo.com>
2020-01-15 10:09:57 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/****************************************
+* Hello.java
+******************************************
+* Brandon Binczak
+* 1/15/2020
+* CMSC 255 Section 003
+******************************************/
+
+public class Hello {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Brandon Binczak");
+		System.out.println("Hello.java");
+		System.out.println("Lab 1");
+		System.out.println("1/15/2020");
+		System.out.println("CMSC 255, Section 003");
+		System.out.println();
+		}
+	}
