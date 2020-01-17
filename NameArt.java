61bb8ca4033754ba4c56ae0df5e8acc39a1e37f5
omegacat <bmbusaf@yahoo.com>
2020-01-17 13:36:30 -0500

    Add files via upload
    Project 1

---
+++ NameArt.java
@@ -0,0 +1,36 @@
+/****************************************
+* NameArt.java
+*****************************************
+* Project Description:
+* Displays my name in ASCII art for CMSC 255 Project number 1
+* Brandon Binczak
+* 1/17/2020
+* CMSC 255 Section 003
+******************************************/
+
+public class NameArt 
+{
+	// 
+	
+	public static void main(String[] args) 
+	{
+		System.out.println();
+		System.out.println("Brandon Binczak");
+		System.out.println("Project 1 -- Name in Code");
+		System.out.println("This project outputs my name in a unique way on the display screen.");
+		System.out.println("01/17/2020");
+		System.out.println("CMSC 255 Section 003");
+		System.out.println();
+		System.out.println();
+		System.out.println("  ________   ________   ________   ________   ________   ________   ________");
+		System.out.println(" /       /| /       /| /       /| /    /  /| /	     /| /       /| /    /  /|");
+		System.out.println("/_______/ |/_______/ |/_______/ |/____/__/ |/_______/ |/_______/ |/____/__/ |");
+		System.out.println("|      |  ||      |  ||      |  ||    | |  ||      |  ||      |  ||    | |  |");
+		System.out.println("|  @   | /||  @   | /||  @   |  ||    | |  ||  |   |  ||   @  |  ||    | |  |");
+		System.out.println("|   ___|/ ||   ___|/ ||  @   |  ||    | |  ||  @   |  ||  @@@ |  ||    | |  |");
+		System.out.println("|      |  ||      |  ||      |  ||  |   |  ||  |   |  ||   @  |  ||  |   |  |");
+		System.out.println("|  @   | / |  |   | / |  |   | / |  |   | / |      | / |      | / |  |   | /");
+		System.out.println("|______|/  |__|___|/  |__|___|/  |__|___|/  |______|/  |______|/  |__|___|/");
+		System.out.println();
+	}
+}
